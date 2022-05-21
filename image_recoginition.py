fa7b953af9301714aabcf3ca49863430a4294bb8
NainaSaid <40417893+NainaSaid@users.noreply.github.com>
2022-05-21 16:35:26 +0500

    add path

--- image_recoginition.py
+++ image_recoginition.py
@@ -5,4 +5,4 @@ import lightgbm as lgb
 import pandas
 
 
-result = I_Recoginizer.find(img_path = "./ime3.jpg", db_path = r"D://Projects//Intership//Face_Recognittion//face_recognition_v4//dataset//", model_name="Facenet", enforce_detection = False)
+result = I_Recoginizer.find(img_path = "./ime3.jpg", db_path = r"D://Projects//Unauthorized_person_Recognittion//Unauthorized_person_recognition_v4//dataset//", model_name="Facenet", enforce_detection = False)
